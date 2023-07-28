9d8b1d483acc074752b5b110bc0d61940ab3c52e
Luis Leno <luizleno15@gmail.com>
2023-07-28 13:55:42 -0300

    adjust

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-hweader">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          pr4
-        </p>
+        <p>pr4</p>
         <a
           className="App-link"
           href="https://reactjs.org"
