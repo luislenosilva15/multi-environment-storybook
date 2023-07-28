c320004501d1c81b600983bb2d522afed7d0581d
Luis Leno <luizleno15@gmail.com>
2023-07-28 16:27:33 -0300

    adjust

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>main test</p>
+        <p>LUIS PAG</p>
         <a
           className="Awpp-link"
           href="https://reactjs.org"
